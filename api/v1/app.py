78c546667a242e593eb881db1e82c4d76f948487
ilasso <1204@holbertonschool.com>
2020-04-29 06:09:04 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,11 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import Flask, Blueprint, make_response, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 app.register_blueprint(app_views)
