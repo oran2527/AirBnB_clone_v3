63884941b2dbc0361f243098a36c80f90bb813c4
oran2527 <orlago250183@gmail.com>
2020-04-29 00:30:33 +0000

    point 8 init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,8 +5,6 @@ create app_view Blueprint with prefix /api/v1
 """
 
 from flask import Flask, Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.cities import *
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,28 +6,11 @@ create api routes:
 /stats: return quantity of tables or clases. method GET
 """
 
+from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
 from models.city import City
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def displayStatus():
-    """
-    """
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def displayStat():
-    """
-    """
-    return jsonify({'amenities': storage.count('Amenity'),
-                    'cities': storage.count('City'),
-                    'places': storage.count('Place'),
-                    'reviews': storage.count('Review'),
-                    'states': storage.count('State'),
-                    'users': storage.count('User')})
+from models.state import State
 
 
 @app_views.route('/states/<text>/cities', methods=['GET'], strict_slashes=False)
