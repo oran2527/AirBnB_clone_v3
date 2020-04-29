90fd7745fe0cb45428c6a80e9a090a160f996110
oran2527 <orlago250183@gmail.com>
2020-04-29 22:31:19 +0000

    point 12 reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -13,6 +13,7 @@ from models.city import City
 from models.state import State
 from models.place import Place
 from models.review import Review
+from models.user import User
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET\
@@ -22,29 +23,22 @@ def displayReviewsByPlace(place_id):
     """
     list_reviews = []
     reviews = storage.all('Review')
-    for key, value in places.items():
+    for key, value in reviews.items():
         if value.place_id == place_id:
             list_reviews.append(value.to_dict())
-    if list_places == []:
+    if list_reviews == []:
         abort(404)
-    else:
-        return jsonify(list_reviews)
+    return jsonify(list_reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def displayReviewbyId(review_id):
     """Return the review by id if not error 404
     """
-    list_reviews = []
-    reviews = storage.all('Review')
-    for key, value in reviews.items():
-        if value.id == review_id:
-            list_reviews.append(value.to_dict())
-            break
-    if list_reviews == []:
+    reviews = storage.get('Review', review_id)
+    if not reviews:
         abort(404)
-    else:
-        return jsonify(list_reviews)
+    return jsonify(reviews.to_dict())
 
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE\
@@ -54,16 +48,13 @@ def deleteReview(review_id):
     """
     list_reviews = {}
     flag = 0
-    reviews = storage.all('Review')
-    for key, value in reviews.items():
-        if value.id == review_id:
-            flag = 1
-            storage.delete(reviews[key])
-            storage.save()
-            break
-    if flag == 0:
+    reviews = storage.get('Review', review_id)
+    if not review:
         abort(404)
-    return jsonify(list_reviews.to_dict()), 200
+    flag = 1
+    storage.delete(reviews)
+    storage.save()
+    return jsonify(list_reviews), 200
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['POST\
@@ -71,22 +62,25 @@ def deleteReview(review_id):
 def createReview(place_id):
     """Create a review for a place if not error 404
     """
-    flag_place_id = 0
+    userid = ""
     review = request.get_json()
     if not review:
         abort(400, {'Not a JSON'})
-    if 'name' not in review:
-        abort(400, {'Missing name'})
-    places = storage.all('Place')
-    text_final = "{}.{}".format('Place', place_id)
-    for key, value in places.items():
-        if key == text_final:
-            flag_place_id = 1
-            break
-    if flag_place_id == 0:
+    if 'user_id' not in review:
+        abort(400, {'Missing user_id'})
+    if 'text' not in review:
+        abort(400, {'Missing text'})
+    for key, value in review.items():
+        if key == 'user_id':
+            userid = value
+    users = storage.get('User', userid)
+    if not users:
+        abort(404)
+    places = storage.get('Place', place_id)
+    if not places:
         abort(404)
     review.update(place_id=place_id)
-    new_review = Reviewe(**review)
+    new_review = Review(**review)
     storage.new(new_review)
     storage.save()
     return jsonify(new_review.to_dict()), 201
@@ -100,18 +94,12 @@ def updateReview(review_id):
     review = request.get_json()
     if not review:
         abort(400, {'Not a JSON'})
-    reviews = storage.all('Review')
-    text_final = "{}.{}".format('Review', review_id)
-    for key, value in reviews.items():
-        if key == text_final:
-            flag_place = 1
-            break
-    if flag_place == 0:
-        abort(404)
     rev = storage.get('Review', review_id)
     if not rev:
         abort(404)
+    ignore = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
     for key, value in review.items():
-        setattr(rev, key, value)
+        if key not in ignore:
+            setattr(rev, key, value)
     storage.save()
     return jsonify(rev.to_dict()), 200
